447de3ab1db693d5f1641d45c2601325f6c0c234
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-04-25 18:26:36 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Pablo Fiallos
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function enterClicked() {
+  // input
+  let age = parseInt(document.getElementById('age').value)
+  // output
+  if (age >= 17) {
+    document.getElementById("user-info").innerHTML =
+      "You can see an R movie alone."
+  } else if (age >= 13) {
+    document.getElementById("user-info").innerHTML =
+      "You can see a PG-13 movie alone."
+  } else if (age >= 5) {
+    document.getElementById("user-info").innerHTML =
+      "You can see a G or PG movie alone."
+  } else {
+    document.getElementById("user-info").innerHTML =
+      "You can see nothing."
+  }
+}
